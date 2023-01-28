91590f3c6caf7cb4aff03765427b985bcaa723b6
Dim Katcher <dim.katcher@gmail.com>
2023-01-28 11:44:36 +0200

    add eslint config && prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,51 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    'env': {
+        es6: true,
+        node: true,
+        jest: true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        project: './tsconfig.json',
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'react-hooks',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        "max-len": ["error", { "code": 120 }],
+        "react/jsx-one-expression-per-line": [1, { "allow": "literal" }],
+        'indent': [
+            'error',
+            2,
+            { SwitchCase: 1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single',
+            { avoidEscape: true }
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'prettier/prettier': 'error',
+    }
 };
